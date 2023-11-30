37eabdc45b75e966633a5810483e9ccd0138ac6a
z0rkuM <oalvarellos@gmail.com>
2023-11-30 11:00:36 +0100

    init

---
+++ src/app/mi-componente/mi-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './mi-componente.component.html',
+  styleUrl: './mi-componente.component.scss'
+})
+export class MiComponenteComponent {
+
+}
